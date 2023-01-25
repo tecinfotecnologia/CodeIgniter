ff76a984a73ef946f300bd12fedec269b4047d8f
Tecinfo Tecnologia <thalesalmeida.tecinfo@gmail.com>
2023-01-25 12:18:23 +0000

    Tradução

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -99,15 +99,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</h1>
 
 		<div id="body">
-			<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+			<p>A página que você está vendo está sendo gerada dinamicamente pelo CodeIgniter.</p>
 
-			<p>If you would like to edit this page you'll find it located at:</p>
+			<p>Se você gostaria de editar esta página, você a encontrará localizada em:</p>
 			<code>application/views/welcome_message.php</code>
 
-			<p>The corresponding controller for this page is found at:</p>
+			<p>O controlador correspondente para esta página é encontrado em:</p>
 			<code>application/controllers/Welcome.php</code>
 
-			<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+			<p>Se você está explorando o CodeIgniter pela primeira vez, você deve começar lendo o <a href="user_guide/">User Guide</a>.</p>
 		</div>
 
 		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
